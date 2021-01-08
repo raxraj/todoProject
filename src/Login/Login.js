6a3501a11801dadbed4b392059647f453b6fd0f8
Ashutosh Kumar <raj.fungus@gmail.com>
2021-01-08 19:35:15 +0530

    Validation

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,23 +1,61 @@
 import './Login.css'
+import axios from 'axios'
 import { Link } from "@reach/router";
+import { useState } from "react";
 
 const Login = (props) => {
+
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState('');
+
+    let loginUser = () => {
+        axios.post('http://127.0.0.1:5500/login',
+            {
+                userName: userName,
+                password: password
+            }
+        )
+            .then((response) => {
+                if (response.data.done) {
+                    setUserName('')
+                    setPassword('')
+                    setMessage("Successfully Logged In!")
+                }
+                else{
+                    setUserName('')
+                    setPassword('')
+                    setMessage(response.data.message)
+                }
+            })
+            
+    }
+
     return (
         <div className="center">
             <h1>Login</h1>
-            <form action method="post">
+            <form>
+            {message}
                 <div className="text_field">
-                    <input type="text" placeholder="Username" required />
+                    <input type="text" placeholder="Username" required
+                        value={userName}
+                        onChange={(event)=>setUserName(event.target.value)}
+                     />
                 </div>
                 <div className="text_field">
-                    <input type="password" placeholder="Password" required />
+                    <input type="password" placeholder="Password"
+                        value = {password}
+                        onChange = {(event)=>setPassword(event.target.value)}
+                     required />
                 </div>
-                <br/>
-                <button className="login-register-btn">Login</button>
+                <br />
+            </form>
+            <button className="login-register-btn"
+                    onClick={loginUser}
+                >Login</button>
                 <div className="signup_link">
                     Not a member? <Link to='/signup'> Sign up </Link>
                 </div>
-            </form>
         </div>
     );
 }
--- src/Login/Signup.js
+++ src/Login/Signup.js
@@ -7,58 +7,69 @@ const SignUp = () => {
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
     const [message, setMessage] = useState('')
-        let registeredUser = () => {
-            axios.post('http://127.0.0.1:5500/register',
+    let registeredUser = () => {
+        axios.post('http://127.0.0.1:5500/register',
             {
-                fullName : fullName,
-                userName : userName,
-                password : password
+                fullName: fullName,
+                userName: userName,
+                password: password
             })
             .then((response) => {
-                if(response.data.done) {
+                if (response.data.done) {
                     /*
                         this.setState({fullName: '', userName: '', password:'', message:'Registere'})
                     */
                     setFullName("")
                     setUserName('')
                     setPassword('')
-                    setMessage("Successfully Registered!") 
+                    setMessage("Successfully Registered!")
+                }
+                else{
+                    if(response.data.message == 'userName'){
+                        setMessage('username is invalid')
+                    }
+                    else if(response.data.message == 'fullName'){
+                        setMessage('fullname should be atleast 3 characters long and atmost 20 char')
+                    }
+                    else{
+                        setMessage('password is invalid')
+                    }
                 }
             })
-            .catch( (error) => {
+            .catch((error) => {
                 console.log(error)
             })
-        }
+    }
     return (
         <div className="center">
             <h1>Register</h1>
             <form>
-            <h3>{message}</h3>
+                <h3>{message}</h3>
                 <div className="text_field">
                     <input id="fullName" type="text" placeholder="FullName"
-                    value = {fullName}
-                    onChange = {(event) => setFullName(event.target.value)}
-                     />
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
                 <div className="text_field">
-                    <input id="userName" type="text" placeholder="Username" 
-                    value = {userName}
-                    onChange = {(event) => setUserName(event.target.value)}
-                     />
+                    <input id="userName" type="text" placeholder="Username"
+                        value={userName}
+                        onChange={(event) => setUserName(event.target.value)}
+                    />
                 </div>
                 <div className="text_field">
-                    <input id="password" type="password" placeholder="Password" 
-                        value = {password}
-                        onChange = {(event) => setPassword(event.target.value)}
+                    <input id="password" type="password" placeholder="Password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
                     />
 
                 </div>
-                <br/>
+                <br />
             </form>
-            <button class='login-register-btn' onClick = {registeredUser}> Register</button>
-                <div className="signup_link">
-                    Already a member? <Link to= '/login'>Login</Link>
-                </div>
+            <button class='login-register-btn' onClick={registeredUser}> Register</button>
+            <div className="signup_link">
+                Already a member? <Link to='/login'>Login</Link>
+            </div>
         </div>
     );
 }
